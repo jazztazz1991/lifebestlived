a842eeb26ff9a11a82e60e5a66280b3e306a1902
Cody Chase <cchase1991@gmail.com>
2017-08-22 08:58:08 -0400

    calender changed to lvlived gmail

--- controllers/lbl_controller.js
+++ controllers/lbl_controller.js
@@ -10,5 +10,12 @@ router.get('/', function(req, res) {
 });
 
 
+router.get('/blog', function(req,res){
+    db.Blogs.findAll({}).then(function(dbTodo) {
+      // We have access to the todos as an argument inside of the callback function
+      res.json(dbTodo);
+    });
+})
+
 // Export routes for server.js to use.
 module.exports = router;
