d829b5e36457b07e1eee5e933a600b338a659b07
Cody Chase <cchase1991@gmail.com>
2017-08-26 08:48:55 -0400

    adjusted events

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,7 +1,7 @@
 var events = [
-  {'Date': new Date(2017, 7, 26), 'Title': 'Project Is Due.'},
+  {'Date': new Date(2017, 7, 29), 'Title': 'Project Is Due.'},
   {'Date': new Date(2017, 8, 28), 'Title': 'Final Project Begins!'},
-  {'Date': new Date(2017, 7, 27), 'Title': 'Hope For Good Grade', 'Link': 'https://bootcampspot-v2.com/'},
+  {'Date': new Date(2017, 7, 30), 'Title': 'Hope For Good Grade', 'Link': 'https://bootcampspot-v2.com/'},
   {'Date': new Date(2017, 7, 24), 'Title': 'Celebrate Bens Birthday', 'Link': 'http://www.ajisushiteppan.com/'},
 ];
 var settings = {};
